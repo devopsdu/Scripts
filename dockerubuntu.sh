deb45e9c330696f1eea531e00d40c2e879c4615a
deraviyam <deraviyam@gmail.com>
2017-07-06 19:35:34 +0530

    Create dockerubuntu.sh

---
+++ dockerubuntu.sh
@@ -0,0 +1,8 @@
+# install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ubuntu
+sudo systemctl status docker
