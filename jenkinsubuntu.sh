15834bc820b2e9aa4c2e0dade0b471b01e33a040
GitHub Developer <deraviyam@gmail.com>
2018-10-27 12:44:00 +0530

    Update jenkinsubuntu.sh

--- jenkinsubuntu.sh
+++ jenkinsubuntu.sh
@@ -1,7 +1,7 @@
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 sudo apt-get update
-sudo apt-get install -y default-jre
+sudo apt install openjdk-8-jdk
 sudo apt-get install jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
