e8a32c5acf5f2e1ffaee14e36cb0ca2fd68e129e
GitHub Developer <deraviyam@gmail.com>
2020-09-24 09:43:10 +0530

    Update jenkinsubuntu.sh

--- jenkinsubuntu.sh
+++ jenkinsubuntu.sh
@@ -1,4 +1,4 @@
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+wget -qO - https://pkg.jenkins.io/debian-stable/jenkins.io.key | suod apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 sudo apt-get update
 sudo apt install openjdk-8-jdk
