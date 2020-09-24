1c276fc48137edd8cd1eb1410b86e466f4a66281
GitHub Developer <deraviyam@gmail.com>
2020-09-24 10:02:21 +0530

    Update jenkinsubuntu.sh

--- jenkinsubuntu.sh
+++ jenkinsubuntu.sh
@@ -1,3 +1,4 @@
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 wget -qO - https://pkg.jenkins.io/debian-stable/jenkins.io.key | suod apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 sudo apt-get update
