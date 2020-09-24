90e072349137e11787d00ff2c16062f337c066fe
GitHub Developer <deraviyam@gmail.com>
2020-09-24 10:09:26 +0530

    Delete jenkinsubuntu.sh

--- jenkinsubuntu.sh
+++
@@ -1,9 +0,0 @@
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
-    /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo add-apt-repository universe
-sudo apt install openjdk-8-jdk
-sudo apt-get install jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
